22659b20a59344417ea5cf218f6da40be6cc3130
Swishhyy <dylan.minatrea@gmail.com>
2025-04-12 18:31:32 -0500

    Project init

---
+++ Plugin.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Exiled.API.Features;
+
+namespace SCE
+{
+    public class Plugin : Plugin<Config>
+    {
+        // Override the plugin's name property.
+        public override string Name => "SCE";
+        // Override the plugin's author property.
+        public override string Author => "Swishhyy";
+        // Override the plugin's version property.
+        public override Version Version => new(1, 0, 0);
+    }
+}
