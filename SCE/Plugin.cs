3bb9ad80a0bcb5cc16c05bfdc6f9a300806341ad
Swishhyy <dylan.minatrea@gmail.com>
2025-04-12 21:58:15 -0500

    Fixed a naming error 💀

--- SCE/Plugin.cs
+++ SCE/Plugin.cs
@@ -10,7 +10,7 @@ namespace SCE
     public class Plugin : Plugin<Config>
     {
         // Override the plugin's name property.
-        public override string Name => "SCR";
+        public override string Name => "SCE";
         // Override the plugin's author property.
         public override string Author => "Swishhyy";
         // Override the plugin's version property.
